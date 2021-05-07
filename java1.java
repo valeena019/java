297052668c6ba945194dfc2ec98c61777dfd7be4
valeena019 <74050930+valeena019@users.noreply.github.com>
2021-05-07 16:13:43 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,17 @@
+public class java1 {
+    public static void main(String args[]){
+        int studentAge =15;
+        double studentGpa=3.45;
+        char studentFirstInitial='k';
+        char studentLastInitial='h';
+        boolean hasPerfectAttendance=true;
+         System.out.println(studentAge);
+         System.out.println(studentGpa);
+         System.out.println(studentFirstInitial);
+         System.out.println(studentLastInitial);
+         System.out.println(hasPerfectAttendance);
+
+
+
+    }
+}
